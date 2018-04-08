177ef8140aff96000b16706a5bdc15aeadab8740
Polina <vslpoulin@gmail.com>
2018-04-08 19:20:09 +0300

    completed(1)

--- src/main/WordsCounter.java
+++ src/main/WordsCounter.java
@@ -7,28 +7,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 class WordsCounter {
-    
+
+    private final static String KEY_WORDS = "private, protected, public, abstract, class, extends, final, implements, interface, " +
+            "native, new, static, strictfp, synchronized, transient, volatile, break, case, continue, default, " +
+            "do, else, for, if, instanceof, return, switch, while, import, package, boolean, byte, char, double, " +
+            "float, int, long, short, assert, catch, finally, throw, throws, try, enum, super, this, void, const, goto";
+
     public void countWordFromFile(String originalFilePath) throws IOException {
         StringBuilder stringBuilder;
-        FileReader fileReader = new FileReader(originalFilePath);
         int a;
         stringBuilder = new StringBuilder();
-        while ((a = fileReader.read()) != -1) {
-            stringBuilder.append((char) a);
+        try (FileReader fileReader = new FileReader(originalFilePath)) {
+            while ((a = fileReader.read()) != -1) {
+                stringBuilder.append((char) a);
+            }
         }
-        fileReader.close();
 
         String[] originalFileDataStringArray = stringBuilder.toString().split(" ");
 
-
-        String keyWords = "private, protected, public, abstract, class, extends, final, implements, interface, " +
-                "native, new, static, strictfp, synchronized, transient, volatile, break, case, continue, default, " +
-                "do, else, for, if, instanceof, return, switch, while, import, package, boolean, byte, char, double, " +
-                "float, int, long, short, assert, catch, finally, throw, throws, try, enum, super, this, void, const, goto";
-
-
         Map<String, Integer> counterMap = new HashMap();
-        for (String string : keyWords.split(", ")) {
+        for (String string : KEY_WORDS.split(", ")) {
             counterMap.put(string, 0);
         }
 
@@ -40,10 +38,12 @@ class WordsCounter {
             }
         }
 
-        FileWriter fileWriter = new FileWriter("keyWordsCount.txt");
-        for (String key : counterMap.keySet()) {
-            fileWriter.write(key + " " + counterMap.get(key).toString() + "\n");
+        try (FileWriter fileWriter = new FileWriter("keyWordsCount.txt")) {
+            for (String key : counterMap.keySet()) {
+                if (!counterMap.get(key).equals(0)) {
+                    fileWriter.write(key + " " + counterMap.get(key).toString() + "\n");
+                }
+            }
         }
-        fileWriter.close();
     }
 }
